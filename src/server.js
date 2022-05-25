459dd2694406b113e060b154e31cbc7fdffdc89a
Haseeb <haseebsaeed2012@gmail.com>
2022-05-25 14:00:10 +0500

    added global error handler middleware

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const eventEmitter = require('./utils/event')
 const { DB_CONNECTION_EVENT } = require('./utils/constants')
 
 const logger = require('./logger')
-
 const cache = require('./routes/cache')
+const errorsMiddlware = require('./middlewares/error')
 
 app.use(express.json());
 
@@ -16,6 +16,9 @@ app.use(express.json());
 app.get('/api/status', async (req, res) => res.status(200).json({ message: "Cache service is up and running." }));
 
 app.use('/api/cache', cache)
+
+/** Error handler middleware */
+app.use(errorsMiddlware);
 
 PORT = process.env.PORT || 80
 
